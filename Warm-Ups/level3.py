d504f998b32afc2b598c6f498bef79bc59fdca2e
Zagox11 <72284724+Zagox11@users.noreply.github.com>
2020-10-21 15:19:55 +0100

    Level 3 solution

--- Warm-Ups/level3.py
+++ Warm-Ups/level3.py
@@ -1,13 +1,24 @@
 """ Hackathon - Level 3 """
 
-def oddish_evenish(x):
-    # Add your solution here. You can use additional functions if need be.
-    # Don't forget to add a DocString for all your functions and comment your code.
-    # Your functions should return values rather than printing the result although you can use printing for testing purposes.
-    return "Your Return Value"
-
-if __name__ == '__main__':
-    # Add any code to test your solution here
-    # As per the example, this should return Oddish
-    print(oddish_evenish(1190))
+def oddish_evenish(number):
+    """ Function to determine if the sum of a number's digits are odd or even.
+    If odd, the number is oddish, and if even the number is evenish. """
     
+    string_form = str(number)   # Converting number to a string
+    list_digits = list(string_form) # Splitting string into a list
+    
+    count = 0   # Declaring the total count. This will become the sum of all digits.
+    
+    for digit in list_digits:   # Loop to go through every digit and add it to the count.
+        digit_integer = int(digit)  # Converting the digit string into an integer
+        count = count + digit_integer   # Adding the digit to the total count
+    
+    if count % 2 == 0:  # Finds if the count is even or not.
+        type = "Evenish"    # If the count is even
+    else:
+        type = "Oddish" # If the count is odd
+    return type # Return type (evenish or oddish)
+
+if __name__ == '__main__':  # Testing it
+    print(oddish_evenish(1190)) # Prints out "Oddish"
+    print(oddish_evenish(523))  # Prints out "Evenish"
